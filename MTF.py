71bc6c85873f95ea3714a582a5c5aa27381ea6c5
wlodawskymichael <wlodawskymichael@yahoo.com>
2017-04-18 16:40:43 -0500

    added code to import the training data

--- MTF.py
+++ MTF.py
@@ -1,3 +1,8 @@
 import tensorflow
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+#import array [784, 55000], each 784 array is a single piece of 
+#training data (there are 55,000) pieces of training data
+matrixOfTestData = mnist.train.images
+
